57df9697666e6907e0aa8e70c29ea0ec85d54443
smt-ttk <53299336+smt-ttk@users.noreply.github.com>
2019-07-27 11:31:19 +0300

    ECommerces

---
+++ ECommerce/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ECommerce.Models;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            using (ECommerceContext eCommerceContext = new ECommerceContext())
+            {
+                List<User> users = eCommerceContext.Users.Include(a => a.Addresses).ToList();
+                List<Address> addresses = eCommerceContext.Addresses.Include(a => a.User).ToList();
+            }
+
+            return View();
+        }
+    }
+}
